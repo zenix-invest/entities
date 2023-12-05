321876a5b11408211a2a1bd22ca4191a1494a7c5
Ivaylo Tsonev <ivaylo.tsonev@outlook.com>
2023-12-05 17:07:29 +0200

    Add patch response and request

--- Sources/Entities/User/User+Account.swift
+++ Sources/Entities/User/User+Account.swift
@@ -32,26 +32,66 @@ public extension User.Account {
         }
     }
     
-    enum List {
+    enum Patch {
+        public struct Request: Codable {
+            public let email: String?
+            public let fullName: String?
+            public let status: Status?
+
+            public init(
+                email: String?,
+                fullName: String?,
+                status: Status?
+            ) {
+                self.email = email
+                self.status = status
+                self.fullName = fullName
+            }
+        }
+        
         public struct Response: Codable {
             public let id: UUID
             public let email: String
-            public let password: String
+            public let fullName: String
+            public let status: Status
+            public let level: Int
+            public let isAdmin: Bool
+            public let isEmailVerified: Bool
+
+            public init(
+                id: UUID,
+                email: String,
+                fullName: String,
+                status: Status,
+                level: Int,
+                isAdmin: Bool,
+                isEmailVerified: Bool
+            ) {
+                self.id = id
+                self.email = email
+                self.status = status
+                self.fullName = fullName
+                self.level = level
+                self.isAdmin = isAdmin
+                self.isEmailVerified = isEmailVerified
+            }
+        }
+    }
+    
+    enum List {
+        public struct Response: Codable {
+            public let id: UUID
             public let fullName: String
             public let status: Status
             public let level: Int
 
             public init(
                 id: UUID,
-                email: String,
-                password: String,
                 fullName: String,
                 status: Status,
                 level: Int
             ) {
                 self.id = id
-                self.email = email
-                self.password = password
                 self.fullName = fullName
                 self.status = status
                 self.level = level
