a71d1f77c67f0d08ee970597e44b7045c9c1f19a
Ivaylo Tsonev <ivaylo.tsonev@outlook.com>
2023-12-10 18:02:07 +0200

    Add create under trading act

--- Sources/Entities/Trading/TradingAccount+Create.swift
+++ Sources/Entities/Trading/TradingAccount+Create.swift
@@ -1,19 +1,21 @@
-public enum Create {
-    public enum Provider: Codable {
-        case etrade, webull
-    }
-    
-    public struct Request: Codable {
-        public let provider: Provider
-        
-        public init(provider: Provider) {
-            self.provider = provider
+public extension TradingAccount {
+    enum Create {
+        public enum Provider: Codable {
+            case etrade, webull
         }
-    }
-    
-    public struct Response: Codable {
-        public init() {
+        
+        public struct Request: Codable {
+            public let provider: Provider
             
+            public init(provider: Provider) {
+                self.provider = provider
+            }
+        }
+        
+        public struct Response: Codable {
+            public init() {
+                
+            }
         }
     }
 }
