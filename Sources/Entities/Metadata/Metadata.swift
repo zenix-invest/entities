c30e49debbddc0d4359e948de54fdc1532d6d686
Ivaylo Tsonev <ivaylo.tsonev@outlook.com>
2024-01-03 10:31:03 +0200

    Add metadata request/response

--- Sources/Entities/Metadata/Metadata.swift
+++ Sources/Entities/Metadata/Metadata.swift
@@ -1,3 +1,19 @@
 import Foundation
 
-public enum Metadata {}
+public enum Metadata {
+    public struct Request: Codable {
+        public let attestation: Data
+        
+        public init(attestation: Data) {
+            self.attestation = attestation
+        }
+    }
+    
+    public struct Response: Codable {
+        public let key: String
+        
+        public init(key: String) {
+            self.key = key
+        }
+    }
+}
