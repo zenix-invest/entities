bf10791f751977e87405031a959caf3e4586eefc
Ivaylo Tsonev <ivaylo.tsonev@outlook.com>
2024-01-05 18:28:10 +0200

    Remove attestation data from request

--- Sources/Entities/AppAttest/Attestation.swift
+++ Sources/Entities/AppAttest/Attestation.swift
@@ -13,20 +13,17 @@ public enum Attestation {
     
     public enum Verification {
         public struct Request: Codable {
-            public let attestation: Data
             public let challengeID: UUID
             public let keyID: Data
             public let teamID: String
             public let bundleID: String
             
             public init(
-                attestation: Data,
                 challengeID: UUID,
                 keyID: Data,
                 teamID: String,
                 bundleID: String
             ) {
-                self.attestation = attestation
                 self.challengeID = challengeID
                 self.keyID = keyID
                 self.teamID = teamID
